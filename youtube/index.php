2c03ed396c878a0b0e123905551645f3ccbb083e
Taufik Nurrohman <t.nurrohman77@gmail.com>
2017-10-31 21:41:15 +0700

    Prevent Nested `<p>` Element(s)

--- youtube/index.php
+++ youtube/index.php
@@ -44,7 +44,7 @@ function fn_youtube_replace($s) {
     ) {
         return $s;
     }
-    return preg_replace('#\bhttps?://(?:www\.)?youtu(?:be\.com/watch\?v=|\.be/)([\w-]+)(\S*?)#i', '<p class="youtube" style="display:block;margin-right:0;margin-left:0;padding:25px 0 56.25%;position:relative;height:0;"><iframe style="display:block;margin:0;padding:0;border:0;position:absolute;top:0;left:0;width:100%;height:100%;" src="//www.youtube.com/embed/$1" allowfullscreen></iframe></p>', $s);
+    return preg_replace('#\bhttps?://(?:www\.)?youtu(?:be\.com/watch\?v=|\.be/)([\w-]+)(\S*?)#i', '<span class="youtube" style="display:block;margin-right:0;margin-left:0;padding:25px 0 56.25%;position:relative;height:0;"><iframe style="display:block;margin:0;padding:0;border:0;position:absolute;top:0;left:0;width:100%;height:100%;" src="//www.youtube.com/embed/$1" allowfullscreen></iframe></span>', $s);
 }
 
 Hook::set([
